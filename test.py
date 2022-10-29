dcbbd9127c63f730e3d1260b93c02aa17da8b4b8
khalid ahmad <khalidahmadghiasi@gmail.com>
2022-10-29 13:50:13 +0430

    python boult in functions

---
+++ test.py
@@ -0,0 +1,118 @@
+# from math import *
+# # type casting
+# number=20
+# number2=str(number)
+# # print(number+'number is') this is error 
+# print('number is:'+number2)
+
+# # absulote value of a number
+# print(abs(-5))
+
+
+# # max number
+# print(max(4,2,8))
+
+# # min number
+# print(min(8,3))
+
+
+# # round off
+# print(round(3.2))
+
+
+# # change number to binary
+# print(bin(128))
+
+# # square roote of number
+# print(sqrt(100))
+
+
+# # input from user
+# name=input('input your Name: ')
+# age=input('enter your age')
+# print('your name is : '+name +'and you are '+age+'years old')
+
+
+
+# sentence=input('enter your sentence: ')
+# word1=input('enter the word to replace :  ')
+# word2=input('enter the word to replace it with: ')
+# print(sentence.replace(word1,word2))
+
+
+
+# lists in python
+countries=['afg','pak','irn']
+print(countries)
+print(countries[1])
+print(countries[0][2])
+print(countries[1:])  #it get us from 1 index to the end of list
+print(countries[-1])
+
+# show the type of list
+print(type(countries))
+print(len(countries))
+
+# list constructor
+countries=list(('afghanistan',33,True))
+print(countries)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
