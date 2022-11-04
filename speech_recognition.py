bc04070986c2f42a365cc09ead20d70d66a89804
khalid ahmad <khalidahmadghiasi@gmail.com>
2022-11-04 10:59:49 +0430

    Speech recognition project in python

---
+++ speech_recognition.py
@@ -0,0 +1,9 @@
+from speech_recognition import Microphone,Recognizer,AudioFile
+import winspeech
+recog=Recognizer()
+mic=Microphone()
+with mic:
+    print('talk')
+    audio =recog.record(mic,3)
+recognized=recog.recognize_google(audio)
+print('you said:  ',recognized)
